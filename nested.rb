e7254aba0d4747ba3622ae3ae1e9827dd9a83605
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 21:11:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,7 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     dennis_new_language = "Assembly"
-     programmer_hash[:dennis_ritchie][:languages] = dennis_new_language
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
      programmer_hash
 end
